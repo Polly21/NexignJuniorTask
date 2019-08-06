a3618acf85848657a3e1f0840cef4d8ed24bf50e
Kirill.Baryshev <baryshev.k.s@mail.ru>
2019-08-06 18:11:58 +0700

    READY???

--- src/main/java/com/nexign/dao/ProductRepository.java
+++ src/main/java/com/nexign/dao/ProductRepository.java
@@ -3,7 +3,6 @@ package com.nexign.dao;
 
 import com.nexign.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -15,13 +14,8 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
 
     List<Product> findAll();
 
-    @Query("SELECT p FROM Product p JOIN FETCH p.productHistories ph " +
-            "WHERE p.id = ?1")
     Optional<Product> findById(Integer id);
 
-    @Query("SELECT p FROM Product p JOIN FETCH p.productHistories ph " +
-            "WHERE p.productName = :name " +
-            "AND p.producer = :producer ")
     Optional<Product> findByProductNameAndProducer(@Param("name") String nameProduct, @Param("producer") String producer);
 
 }
